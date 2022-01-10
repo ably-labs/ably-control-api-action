c3a78b6ac1c0952eb9113d7fe5a06b793b274b06
Marc Duiker <marcduiker@users.noreply.github.com>
2022-01-10 09:14:39 +0100

    Change to use Axios config syntax

--- index.js
+++ index.js
@@ -7,19 +7,20 @@ try {
   const appName = core.getInput('app-name');
   console.log(`Ably app to create: ${appName}!`);
   const createAppUrl = `https://control.ably.net/v1/accounts/${accountId}/apps`;
-  const data = {
-    "name": appName,
-    "status": "enabled",
-    "tlsOnly": true,
-    "fcmKey": null,
-    "apnsCertificate": null,
-    "apnsPrivateKey": null,
-    "apnsUseSandboxEndpoint": false
-  };
-  const config = {
-    headers: { Authorization: `Bearer ${controlApiKey}` },
-  };
-  axios.post(createAppUrl, data, config).then(function (response) {
+  axios({
+    method: 'post',
+    url: createAppUrl,
+    headers: { 'Authorization': `Bearer ${controlApiKey}` },
+    data: {
+      "name": appName,
+      "status": "enabled",
+      "tlsOnly": true,
+      "fcmKey": null,
+      "apnsCertificate": null,
+      "apnsPrivateKey": null,
+      "apnsUseSandboxEndpoint": false
+    }
+  }).then(function (response) {
     core.setOutput("app-id", response.id);
   });
 } catch (error) {
