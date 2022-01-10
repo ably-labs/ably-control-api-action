ae0761d9827636dc9dcee0f95ece04bf217290e2
Marc Duiker <marcduiker@users.noreply.github.com>
2022-01-10 09:09:46 +0100

    Fix Axios POST

--- index.js
+++ index.js
@@ -7,19 +7,19 @@ try {
   const appName = core.getInput('app-name');
   console.log(`Ably app to create: ${appName}!`);
   const createAppUrl = `https://control.ably.net/v1/accounts/${accountId}/apps`;
+  const data = {
+    "name": appName,
+    "status": "enabled",
+    "tlsOnly": true,
+    "fcmKey": null,
+    "apnsCertificate": null,
+    "apnsPrivateKey": null,
+    "apnsUseSandboxEndpoint": false
+  };
   const config = {
     headers: { Authorization: `Bearer ${controlApiKey}` },
-    data: {
-      "name": appName,
-      "status": "enabled",
-      "tlsOnly": true,
-      "fcmKey": null,
-      "apnsCertificate": null,
-      "apnsPrivateKey": null,
-      "apnsUseSandboxEndpoint": false
-    }
   };
-  axios.post(createAppUrl, config).then(function (response) {
+  axios.post(createAppUrl, data, config).then(function (response) {
     core.setOutput("app-id", response.id);
   });
 } catch (error) {
