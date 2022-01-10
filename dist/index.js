d27308616a44174f54f7fe8f0e33b7289cda3204
Marc Duiker <marcduiker@users.noreply.github.com>
2022-01-10 11:44:00 +0100

    Add updated build version

--- dist/index.js
+++ dist/index.js
@@ -4744,6 +4744,7 @@ try {
   const appName = core.getInput('app-name');
   console.log(`Ably app to create: ${appName}!`);
   const createAppUrl = `https://control.ably.net/v1/accounts/${accountId}/apps`;
+  const getApsUrl = `https://control.ably.net/v1/accounts/${accountId}/apps`;
   axios({
     method: 'post',
     url: createAppUrl,
@@ -4757,8 +4758,24 @@ try {
       "apnsPrivateKey": null,
       "apnsUseSandboxEndpoint": false
     }
-  }).then(function (response) {
+  })
+  .then(function (response) {
     core.setOutput("app-id", response.data.id);
+  })
+  .catch(function (error) {
+    if (error.response.status === 422) {
+      // App with the exact name already exists.
+      // Get the app and return its id.
+      axios({
+        method: 'get',
+        url: getApsUrl,
+        headers: { 'Authorization': `Bearer ${controlApiKey}` },
+      })
+      .then(function (response) {
+        let app = response.data.filter(app => app.name.toLowerCase() === appName.toLowerCase())[0];
+        core.setOutput("app-id", app.id);
+      });
+    }
   });
 } catch (error) {
   core.setFailed(error.message);
