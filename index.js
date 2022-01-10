e21e3583ec986cad7cd286f6a474ef44e7d634e2
Marc Duiker <marcduiker@users.noreply.github.com>
2022-01-10 13:55:08 +0100

    Wrap in Promise

--- index.js
+++ index.js
@@ -8,51 +8,53 @@ try {
   const createKey = core.getBooleanInput('create-key');
   const keyName = core.getInput('key-name');
   const keyCapabilities = core.getInput('key-capabilities');
-  const appId = createApp(accountId, controlApiKey, appName);
-  if (createKey) {
-    core.info(`Creating an API key for app: ${appId}.`);
-    createApiKey(appId, controlApiKey, keyName, keyCapabilities);
-  }
+  createApp(accountId, controlApiKey, appName).then((appId) =>  {
+    if (createKey) {
+      core.info(`Creating an API key for app: ${appId}.`);
+      createApiKey(appId, controlApiKey, keyName, keyCapabilities);
+    }
+  })
 } catch (error) {
   core.setFailed(error.message);
 }
 
-function createApp(accountId, controlApiKey, appName)
-{
-  const appUrl = `https://control.ably.net/v1/accounts/${accountId}/apps`;
-  axios({
-    method: 'post',
-    url: appUrl,
-    headers: { 'Authorization': `Bearer ${controlApiKey}` },
-    data: {
-      "name": appName,
-      "status": "enabled",
-      "tlsOnly": true,
-      "fcmKey": null,
-      "apnsCertificate": null,
-      "apnsPrivateKey": null,
-      "apnsUseSandboxEndpoint": false
-    }
-  })
-  .then(function (response) {
-    core.setOutput("app-id", response.data.id);
-    return response.data.id;
-  })
-  .catch(function (error) {
-    if (error.response.status === 422) {
-      // App with the exact name already exists.
-      // Get the app and return its id.
-      axios({
-        method: 'get',
-        url: appUrl,
-        headers: { 'Authorization': `Bearer ${controlApiKey}` },
-      })
-      .then(function (response) {
-        let app = response.data.filter(app => app.name.toLowerCase() === appName.toLowerCase())[0];
-        core.setOutput("app-id", app.id);
-        return app.id;
-      });
-    }
+const createApp = (accountId, controlApiKey, appName) => {
+  return new Promise(resolve => {
+    const appUrl = `https://control.ably.net/v1/accounts/${accountId}/apps`;
+    axios({
+      method: 'post',
+      url: appUrl,
+      headers: { 'Authorization': `Bearer ${controlApiKey}` },
+      data: {
+        "name": appName,
+        "status": "enabled",
+        "tlsOnly": true,
+        "fcmKey": null,
+        "apnsCertificate": null,
+        "apnsPrivateKey": null,
+        "apnsUseSandboxEndpoint": false
+      }
+    })
+    .then(function (response) {
+      core.setOutput("app-id", response.data.id);
+      resolve(response.data.id);
+    })
+    .catch(function (error) {
+      if (error.response.status === 422) {
+        // App with the exact name already exists.
+        // Get the app and return its id.
+        axios({
+          method: 'get',
+          url: appUrl,
+          headers: { 'Authorization': `Bearer ${controlApiKey}` },
+        })
+        .then(function (response) {
+          let app = response.data.filter(app => app.name.toLowerCase() === appName.toLowerCase())[0];
+          core.setOutput("app-id", app.id);
+          resolve(app.id);
+        });
+      }
+    });
   });
 }
 
